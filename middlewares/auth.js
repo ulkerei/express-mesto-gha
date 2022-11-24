842222172aea8e7c4a570a3f2e17ed8f75a2a060
Юлка Торшенко <ulka.torshenko@gmail.com>
2022-11-24 18:44:10 +0100

    feat: 14 sprint.5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,13 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+ /* const { authorization } = req.headers;
   if (!authorization) {
     throw new UnauthorizedError('Необходима авторизация.');
   }
-  const token = authorization.replace(/^Bearer*\s*/i, '');
-  let payload;
+  const token = authorization.replace(/^Bearer*\si, ''); */
   try {
-    payload = jwt.verify(token, '50f9baa64b85bd4b0c60974fd6e5d1c426cd07f7d3de2d5d1665a1aa038ca31e');
+    const payload = jwt.verify(res.cookie.jwt, '50f9baa64b85bd4b0c60974fd6e5d1c426cd07f7d3de2d5d1665a1aa038ca31e');
     req.user = { _id: payload._id };
     next();
   } catch (err) {
