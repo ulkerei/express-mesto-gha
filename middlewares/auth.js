14787fa2125fad3b7bb67d0a7a393df60f34d710
Юлка Торшенко <ulka.torshenko@gmail.com>
2022-11-24 18:31:00 +0100

    feat: 14 sprint.4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     payload = jwt.verify(token, '50f9baa64b85bd4b0c60974fd6e5d1c426cd07f7d3de2d5d1665a1aa038ca31e');
-    req.user = payload;
+    req.user = { _id: payload._id };
     next();
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
